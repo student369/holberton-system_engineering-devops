9310111ac92e0fd69444d9aafc2abbff9bb674af
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-05 20:17:58 -0500

    Adding the 102-zombie.c file.

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - An infinite loop
+ *
+ * Return: Alrays 0.
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+/**
+ * main - PID
+ *
+ * Return: Alrays 0.
+ */
+int main(){
+	pid_t pids[5];
+	int i;
+
+	/*
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() ==0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			infinite_while();
+			_exit(0);
+		}
+	}	
+	for (i = 0; i < 5; i++)
+		wait(NULL);
+	*/
+	for (i = 4; i >= 0; --i)
+	{
+		pids[i] = fork();
+		if(pids[i] == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			infinite_while();
+			_exit(0);
+		}
+	}
+	for (i = 4; i >=0; --i)
+		waitpid(pids[i], NULL, 0);
+	return (0);
+}
