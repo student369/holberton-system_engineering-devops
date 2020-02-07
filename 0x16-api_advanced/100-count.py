e315f0439afb04c59a26eb448a45984b786b6c03
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-06 23:27:39 -0500

    Adding the 100-count.py file.

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that get some results of an API using recursion.
+"""
+import requests
+import recurse
+
+
+def count_words(subreddit, word_list):
+    wl = word_list.split()
+    return (1)
